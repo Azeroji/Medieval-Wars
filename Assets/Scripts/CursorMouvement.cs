633b5e2ea636284e7a52095a82fecf51af50dc98
Amine Kadoum <ma_kadoum@esi.dz>
2024-04-03 00:02:30 +0100

    Cursor mouvement

--- Assets/Scripts/CursorMouvement.cs
+++ Assets/Scripts/CursorMouvement.cs
@@ -4,20 +4,61 @@ using UnityEngine;
 
 public class CursorMouvement : MonoBehaviour
 {
-  [SerializeField] private Rigidbody2D cursorRb;
-  private float speed = 5f;
+  // [SerializeField] private Rigidbody2D cursorRb;
+  // private float speed = 5f;
   // Start is called before the first frame update
+  Vector3 newPosition;
   void Start()
   {
+    newPosition = transform.position;
 
   }
 
   // Update is called once per frame
   void Update()
   {
-    float directionx = Input.GetAxis("Horizontal");
-    float directiony = Input.GetAxis("Vertical");
-    cursorRb.velocity = new Vector2(directionx * speed, directiony * speed);
+    if (Input.GetKeyDown(KeyCode.LeftArrow))
+    {
 
+      if (newPosition.x > -7.51f) //f for float
+      {
+        newPosition.x -= 1;
+
+      }
+    }
+    else if (Input.GetKeyDown(KeyCode.RightArrow))
+    {
+
+      if (newPosition.x < 7.49f)
+      {
+        newPosition.x += 1;
+
+      }
+    }
+
+    if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
+
+      if (newPosition.y > -3.82f)
+      {
+        newPosition.y -= 1;
+
+      }
+
+    }
+    else if (Input.GetKeyDown(KeyCode.UpArrow))
+    {
+
+      if (newPosition.y < 4.18f)
+      {
+        newPosition.y += 1;
+      }
+    }
+
+    transform.position = newPosition;
   }
+
+  // cursorRb.velocity = new Vector2(directionx * speed, directiony * speed);
+
+
 }
