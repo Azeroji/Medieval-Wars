1aa47a50bc4bd5ce2c815023b4179614a51be6ba
Azeroji <daliladimi33@gmail.com>
2024-04-05 06:20:34 +0100

    Added TilemapGenerator

---
+++ Assets/Scripts/Terrain.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public enum TerrainType {
+    Plaine,
+    Montagne,
+    Foret,
+    Cave,
+    Marais,
+    Ocean,
+    VillageNeutre,
+    QgBleu,
+    VillageBleu,
+    BaraqueBleu,
+    PortBleu,
+    AtelierBleu,
+    QgRouge,
+    VillageRouge,
+    BaraqueRouge,
+    PortRouge,
+    AtelierRouge
+}
+
+[System.Serializable]
+public class Terrain {
+    public TerrainType terrainType;
+    public int movementCost;
+    public int defenseBonus;
+    // Add more attributes as needed
+    public Terrain(TerrainType terrainType, int movementCost, int defenseBonus) {
+        this.terrainType = terrainType;
+        this.movementCost = movementCost;
+        this.defenseBonus = defenseBonus;
+        // Initialize other attributes here if needed
+    }
+}
